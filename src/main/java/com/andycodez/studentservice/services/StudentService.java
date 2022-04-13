352d768ba09050e457921bfb2fe21edce932f3eb
Andrew Owino <andystudy4@gmail.com>
2022-04-13 12:48:24 +0300

    Write integration test for service layer

---
+++ src/main/java/com/andycodez/studentservice/services/StudentService.java
@@ -0,0 +1,19 @@
+package com.andycodez.studentservice.services;
+
+import com.andycodez.studentservice.model.entities.Student;
+import com.andycodez.studentservice.model.repositories.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student getStudentById(long id) {
+        return this.studentRepository.findById(id).orElse(null);
+    }
+}
---
+++ src/test/java/com/andycodez/studentservice/StudentServiceTest.java
@@ -0,0 +1,35 @@
+package com.andycodez.studentservice;
+
+import com.andycodez.studentservice.model.entities.Student;
+import com.andycodez.studentservice.model.repositories.StudentRepository;
+import com.andycodez.studentservice.services.StudentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Transactional
+public class StudentServiceTest {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    private StudentService studentService;
+
+    @DisplayName("Get student by Id returns Student")
+    @Test
+    void testGetStudentByIdForSavedStudent_returnsStudent() {
+        Student savedStudent = this.studentRepository.save(new Student("Jeff", true, 50));
+
+        Student retrievedStudent = this.studentService.getStudentById(savedStudent.getId());
+
+        then(savedStudent).isEqualTo(retrievedStudent);
+        then(retrievedStudent.getName()).isEqualTo("Jeff");
+        then(retrievedStudent.getId()).isNotNull();
+    }
+}
